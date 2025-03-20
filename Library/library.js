01e7a53064943795d095862310ec385c6fe665aa
ann.ka.zielinska <ann.ka.zielinska@gmail.com>
2025-03-20 22:15:22 +0100

    added form with adding books to library

--- Library/library.js
+++ Library/library.js
@@ -1,16 +1,39 @@
 const button = document.getElementById("button");
-
+const libraryContainer = document.getElementById("libraryContainer");
 const myLibrary = [];
+const showBtn = document.getElementById("show-dialog");
+const dialog = document.getElementById("dialog");
+const closeBtn = dialog.querySelector("#closeBtn");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
+const dialogForm = document.getElementById("dialogForm");
 
-function Book(name, author) {
-  this.name = name;
+function Book(title, author, pages, read) {
+  this.title = title;
   this.author = author;
+  this.pages = pages;
+  this.read = read;
   this.id = crypto.randomUUID();
 }
 function addBookToLibrary() {
-  const book1 = new Book("Harry Potter", "J.K. Rowling", 1);
-  myLibrary.push(book1);
-  console.log(myLibrary);
+  const book = new Book(title.value, author.value, pages.value, read.value);
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("bookCard");
+  libraryContainer.appendChild(bookCard);
+  bookCard.innerHTML = `<h2>${book.title}</h2><p>${book.author}</p><p>${book.pages}</p>`;
 }
 
-button.addEventListener("click", addBookToLibrary);
+showBtn.addEventListener("click", () => {
+  dialogForm.reset();
+  dialog.showModal();
+  document.body.style.opacity = "30%";
+});
+
+dialogForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  dialog.close();
+  document.body.style.opacity = "100%";
+  addBookToLibrary();
+});
